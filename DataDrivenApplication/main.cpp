069ce046a021e860336cef26aa81ee66289c8b0d
alturkabuterimon <44056838+alturkabuterimon@users.noreply.github.com>
2018-12-29 10:21:36 +0000

    Displaying and Counting all Tweets in file
    Not sure if its necessary to display all the tweets vs just counting them. There's a lot of tweets.

--- DataDrivenApplication/main.cpp
+++ DataDrivenApplication/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 
 int main()
 {
+	int option; 
+	int numOfTweets = 0;
+	string data;
+	ifstream inFile;
+	inFile.open("sampleTweets.csv");
 
+	cout << "Welcome to this Data Driven Application." << endl;
+	cout << "Please select an option:" << endl;
+	cout << "" << endl;
+	cout << "1: Display all tweets." << endl;
+
+	cin >> option;
+
+	if (option == 1)
+	{
+		if (inFile.good())
+		{
+			cout << "Reading from file." << endl;
+			while (!inFile.eof())
+			{
+				getline(inFile, data);
+				cout << data << endl;
+				numOfTweets++;
+			}
+			cout << "The total number of tweets is: " << numOfTweets << endl;
+		}
+	}
+
+	system("Pause");
 	return 0;
 }
