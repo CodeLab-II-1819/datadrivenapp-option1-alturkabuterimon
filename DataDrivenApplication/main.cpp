b097a32d33a6e17e412363250a4042ee12361240
alturkabuterimon <44056838+alturkabuterimon@users.noreply.github.com>
2018-12-30 12:14:25 +0000

    Got "Count the number of times the words money is mentioned" working
    Simpler than I expected.

--- DataDrivenApplication/main.cpp
+++ DataDrivenApplication/main.cpp
@@ -6,8 +6,12 @@ using namespace std;
 int main()
 {
 	int option; 
+	int offset;
 	int numOfTweets = 0;
-	string data;
+	string data, searchTerm;
+	string option2Word = "money";
+	string option3Word = "politics";
+	string line; //Get the line from the file
 	ifstream inFile;
 	inFile.open("sampleTweets.csv");
 
@@ -15,7 +19,7 @@ int main()
 	cout << "Please select an option:" << endl;
 	cout << "" << endl;
 	cout << "1: Count all tweets." << endl;
-	cout << "2: Count all tweets containing the word 'Politics.'" << endl;
+	cout << "2: Count all tweets containing the word 'Money.'" << endl;
 
 
 	cin >> option;
@@ -39,7 +43,15 @@ int main()
 			cout << "Reading from file." << endl;
 			while (!inFile.eof())
 			{
-
+				while (getline(inFile, line))
+				{
+					if ((offset = line.find(option2Word, 0)) != string::npos)
+					{
+						numOfTweets++;
+					}
+					
+				}
+				cout << "The total number of tweets containing the word 'Money' is: " << numOfTweets << endl;
 			}
 		}
 	}
