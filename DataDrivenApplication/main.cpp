cef0cc4b7a36a7b175b07dd9fb314415f55f0d2d
alturkabuterimon <44056838+alturkabuterimon@users.noreply.github.com>
2019-01-21 13:57:05 +0000

    Fixed Restart Function, Cut down on function code, broke printTweets function
    Need to fix print at home.
    Need to add in Validation using cin.fail

--- DataDrivenApplication/main.cpp
+++ DataDrivenApplication/main.cpp
@@ -9,72 +9,75 @@ using namespace std;
 //Functions
 
 //Function to count tweets
-int countTweets(ifstream &inFile, int numOfTweets, int offset, string toUpper, string toLower, string line)
+int countTweets(int numOfTweets, int offset, string toUpper, string toLower)
 {
+	ifstream inFile;
+	string line;
+
 	inFile.open("sampleTweets.csv");
 	if (inFile.good())
 	{
 		cout << "Reading from file." << endl;
 		while (!inFile.eof())
 		{
-			while (getline(inFile, line))
+			getline(inFile, line);
+			if (((offset = line.find(" " + toLower + " ", 0)) != string::npos) || ((offset = line.find(" " + toUpper + " ", 0)) != string::npos))  //Place a space before and after the word to stop any potential words within words
+				//This line finds the word is on the line and saves that line to the variable line, it finds both upper and lowercase values.
 			{
-				if (((offset = line.find(" " + toLower + " ", 0)) != string::npos) || ((offset = line.find(" " + toUpper + " ", 0)) != string::npos))  //Place a space before and after the word to stop any potential words within words
-					//This line finds the word is on the line and saves that line to the variable line, it finds both upper and lowercase values.
-				{
-					numOfTweets++;
-				}
-				
+				numOfTweets++;
 			}
 		}
+		cout << "The total number of tweets is: " << numOfTweets << endl;
 	}
 	return numOfTweets;
 	inFile.close();
 }
 
+
 //Function to print tweets
-void printTweets(ifstream &inFile, int numOfTweets, int offset, string toUpper, string toLower, string line)
+void printTweets(int numOfTweets, int offset, string toUpper, string toLower)
 {
+	ifstream inFile;
+	string line; 
+
 	inFile.open("sampleTweets.csv");
 	if (inFile.good())
 	{
 		cout << "Reading from file." << endl;
 		while (!inFile.eof())
 		{
-			while (getline(inFile, line))
+			getline(inFile, line);
+			if (((offset = line.find(" " + toLower + " ", 0)) != string::npos) || ((offset = line.find(" " + toUpper + " ", 0)) != string::npos))  
 			{
-				if (((offset = line.find(" " + toLower + " ", 0)) != string::npos) || ((offset = line.find(" " + toUpper + " ", 0)) != string::npos))  
-				{
-					numOfTweets++;
-					cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl; //makes it look neater 
-					cout << line << endl; //Print the tweet
+				numOfTweets++;
+				cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl; //makes it look neater 
+				cout << line << endl; //Print the tweet
 
-				}
-				
 			}
-			cout << "The total number of tweets containing the word 'Paris' is: " << numOfTweets << endl; //Print out the number of tweets
+			cout << "The total number of tweets containing the word: " << toUpper << " is: " << numOfTweets << endl; //Print out the number of tweets
 		}
 	}
 	inFile.close();
 }
 
 //Function to search by date
-void searchByDate(ifstream &inFile, int numOfTweets, int offset, string date, string line)
+void searchByDate(int numOfTweets, int offset, string date)
 {
+	ifstream inFile;
+	string line; 
+
 	inFile.open("sampleTweets.csv");
 	if (inFile.good())
 	{
 		cout << "Reading from file." << endl;
 		while (!inFile.eof())
 		{
-			while (getline(inFile, line))
+			getline(inFile, line);
+			if (offset = line.find(date, 0) != string::npos)
 			{
-				if (offset = line.find(date, 0) != string::npos)
-				{
-					numOfTweets++;
-					cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-					cout << line << endl;
-				}
+				numOfTweets++;
+				cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+				cout << line << endl;
 			}
 			cout << "The total number of tweets from " << date << " are: " << numOfTweets << endl;
 		}
@@ -82,10 +85,9 @@ void searchByDate(ifstream &inFile, int numOfTweets, int offset, string date, st
 	inFile.close();
 }
 
-//Function to check for validation errors
 
 //Function to restart the program
-/*bool restartApplication(bool restart)
+int restartApplication(int restart)
 {
 	char restartOption;
 	cout << "Would you like to restart the program? (Y/N)" << endl;
@@ -93,15 +95,18 @@ void searchByDate(ifstream &inFile, int numOfTweets, int offset, string date, st
 
 	if (restartOption == 'Y' || restartOption == 'y')
 	{
-		restart == true;
+		restart = 0;
+		system("CLS");
 		return restart;
 	}
 	else if (restartOption == 'N' || restartOption == 'n')
 	{
-		restart == false;
+		restart = 1;
+		cout << "The application will now close." << endl; 
 		return restart;
+		exit(0);
 	}
-}*/
+}
 
 int main()
 {
@@ -113,12 +118,11 @@ int main()
 	string searchTerm, date;
 	string line; //Get the line from the file
 
-	bool restart = false;
+	int restart = 0;
 
 	ifstream inFile;
 
-
-
+	do {
 
 		cout << "Welcome to this Data Driven Application." << endl;
 		cout << "Please select an option:" << endl;
@@ -143,70 +147,75 @@ int main()
 		if (option == 1)
 		{
 			system("CLS");
-			if (inFile.good()) //If the file is found
-			{
-				cout << "Reading from file." << endl;
-				while (!inFile.eof()) //If before the end of the file
-				{
-					numOfTweets++; 
-				}
-				cout << "The total number of tweets is: " << numOfTweets << endl;
-				system("Pause");
-				cout << endl;
-			
-			}
+			countTweets(numOfTweets, offset, "", "");
+			cin >> count; 
+			cout << endl;
+			restartApplication(restart);
 		}
 
 		//Option 2
 		else if (option == 2)
 		{
-			cout << "The total number of tweets containing the word 'Money' is: " << countTweets(inFile, numOfTweets, offset, "Money", "money", line) << endl;
-			system("Pause");
+			system("CLS");
+			countTweets(numOfTweets, offset, "Money", "money");
+			cout << endl;
+			restartApplication(restart);
 
 		}
 
 		//Option 3
 		else if (option == 3)
 		{
-			cout << "The total number of tweets containing the word 'Politics' is: " << countTweets(inFile, numOfTweets, offset, " Politics ", " politics ", line) << endl;
-			system("Pause");
+			system("CLS");
+			countTweets(numOfTweets, offset, "Politics", "politics");
+			cout << endl;
+			restartApplication(restart);
 		}
 
 		//Option 4
 		else if (option == 4)
 		{
-			printTweets(inFile, numOfTweets, offset, " Paris ", " paris ", line);
-			system("Pause");
+			system("CLS");
+			printTweets(numOfTweets, offset, " Paris ", " paris ");
+			cout << endl;
+			restartApplication(restart);
 		}
 
 		//Option 5
 		else if (option == 5)
 		{
-			printTweets(inFile, numOfTweets, offset, " Dreamworks ", " dreamworks ", line);
-			system("Pause");
+			system("CLS");
+			printTweets(numOfTweets, offset, " Dreamworks ", " dreamworks ");
+			cout << endl;
+			restartApplication(restart);
 		}
 
 		//Option 6
 		else if (option == 6)
 		{
-			printTweets(inFile, numOfTweets, offset, " Uber ", " uber ", line);
-			system("Pause");
+			printTweets(numOfTweets, offset, " Uber ", " uber ");
+			cout << endl;
+			restartApplication(restart);
 		}
 
 		//Option 7
 		else if (option == 7)
 		{
-			printTweets(inFile, numOfTweets, offset, " Cat ", " cat ", line);
-			system("Pause");
+			system("CLS");
+			printTweets(numOfTweets, offset, " Cat ", " cat ");
+			cout << endl;
+			restartApplication(restart);
 		}
 
 		//Option 8
 		else if (option == 8)
 		{
+			system("CLS");
 			cout << "Please enter the date you want to search for (DD/MM/YYYY)" << endl;
 			cin >> date;
-			searchByDate(inFile, numOfTweets, offset, date, line);
-			system("Pause");
+			searchByDate(numOfTweets, offset, date);
+			cout << endl;
+			restartApplication(restart);
 
 		}
 		else if (option == 9)
@@ -227,14 +236,18 @@ int main()
 					}
 				}
 				cout << "The total number of tweets containing " << searchTerm << " is: " << numOfTweets << endl;
-				system("Pause");
+				cout << endl;
+				restartApplication(restart);
 			}
 		}
 		else if (option == 10)
 		{
-			printTweets(inFile, numOfTweets, offset, " Rabbit ", " rabbit ", line);
-			system("Pause");
+			system("CLS");
+			printTweets(numOfTweets, offset, " Rabbit ", " rabbit ");
+			cout << endl;
+			restartApplication(restart);
 
 		}
+	}while (restart == 0);
 }
 
